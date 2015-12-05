cd17ba321c8b439c6fda40d46352ac9622ba7d87
Dan Schwartz <dpschwartz@gmail.com>
2015-12-04 16:22:38 -0800

    Create JSON object for new post.

--- post.js
+++ post.js
@@ -1,8 +1,26 @@
-$('.newPost > p > *').on('focusout', function() {
+$('.newPost > p > *').on('keyup', function() {
   console.log(this.name);
   if (this.name == 'authorUrl') {
     $('.author').html('<a href="'+this.value+'">'+$('input[name="author"]')[0].value+'</a>');
   } else {
     $('.'+this.name).text(this.value);
   };
+});
+
+var postObject = {};
+
+$('input[name="publish"]').on('click', function() {
+  if ($(this).is(':checked')) {
+    var elements = $('.newPost > p > *');
+    for (var i = 0; i < elements.length; i++) {
+      postObject[elements[i].name] = elements[i].value;
+    }
+    var date = new Date();
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+    date = y + '-' + m + '-' + d;
+    postObject.publishedOn = date;
+    $('#target').text(JSON.stringify(postObject));
+  }
 });

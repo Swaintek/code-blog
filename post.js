6d1c48bdd2ca6156146915865eed8ad3b0218a22
Dan Schwartz <dpschwartz@gmail.com>
2015-12-04 17:17:09 -0800

    Add marked, try to add highlight.

--- post.js
+++ post.js
@@ -1,19 +1,19 @@
 $('.newPost > p > *').on('keyup', function() {
-  console.log(this.name);
   if (this.name == 'authorUrl') {
     $('.author').html('<a href="'+this.value+'">'+$('input[name="author"]')[0].value+'</a>');
+  } else if (this.name =='body') {
+    $('.'+this.name).html('<pre><code>'+marked(this.value)+'</code></pre>');
   } else {
     $('.'+this.name).text(this.value);
   };
 });
 
-var postObject = {};
-
 $('input[name="publish"]').on('click', function() {
   if ($(this).is(':checked')) {
+    var postObject = {};
     var elements = $('.newPost > p > *');
     for (var i = 0; i < elements.length; i++) {
-      postObject[elements[i].name] = elements[i].value;
+      postObject[elements[i].name] = marked(elements[i].value);
     }
     var date = new Date();
     var y = date.getFullYear();

da296e4074ef3012afe2ad7cbb222ecb9f5a053f
David Swaintek <dswaintek@gmail.com>
2015-12-08 20:23:04 -0800

    Added CSS

--- articles.js
+++ articles.js
@@ -1,15 +1,15 @@
 var articleBuilder = function (i) {
   // Grab the template script
-  var theTemplateScript = $("#articleTemplate").html();
+  var theTemplateScript = $('#articleTemplate').html();
   // Compile the template
   var theTemplate = Handlebars.compile(theTemplateScript);
   // Define data object
   var context={
-    "title" : rawData[i].title,
-    "authorURL" : rawData[i].authorURL,
-    "author" : rawData[i].author,
-    "age" : Math.floor((new Date - new Date(rawData[i].publishedOn))/86400000)+" Days Old",
-    "body" : rawData[i].body
+    'title' : rawData[i].title,
+    'authorURL' : rawData[i].authorURL,
+    'author' : rawData[i].author,
+    'age' : Math.floor((new Date - new Date(rawData[i].publishedOn))/86400000)+' Days Old',
+    'body' : rawData[i].body
   };
   // Pass data to the template
   var theCompiledHtml = theTemplate(context);
